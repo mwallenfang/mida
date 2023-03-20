5eadfce68c00430a2399d09c19ef23129473a2ab
RitzGit <marvin@wallenfang.de>
2023-03-20 16:06:32 +0100

    Camera tilemap gui basics

---
+++ scenes/Camera2D.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+var current_level = 0
+var move_pos = 0
+var moving_screen = false
+var old_x = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("bam"):
+		current_level += 1
+		old_x = position.x
+		moving_screen = true
+		
+	if moving_screen:
+		position.x = lerp(old_x, old_x + 512, move_pos*move_pos*move_pos)
+		move_pos += delta
+		
+		if move_pos > 1:
+			moving_screen = false
+			position.x = old_x + 512
+			move_pos = 0
